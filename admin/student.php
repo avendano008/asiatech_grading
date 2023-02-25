4af295da93d278e87c4df7708b581bd8148a9be1
Melchor Callos <melchorcallos04@gmail.com>
2023-02-25 15:39:52 +0800

    Commit overview and settings
    Commit overview and settings in admit and changed student

--- admin/student.php
+++ admin/student.php
@@ -20,6 +20,9 @@ if(!isset($_SESSION['email'])){
 		<!--begin::Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 		<!--end::Fonts-->
+		<!--begin::Page Vendor Stylesheets(used by this page)-->
+		<link href="../assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Page Vendor Stylesheets-->
 		<!--begin::Global Stylesheets Bundle(used by all pages)-->
 		<link href="../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 		<link href="../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
@@ -603,11 +606,9 @@ if(!isset($_SESSION['email'])){
 														<input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_table_users .form-check-input" value="1" />
 													</div>
 												</th>
-												<th class="min-w-125px">User</th>
-												<th class="min-w-125px">Role</th>
-												<th class="min-w-125px">Last login</th>
-												<th class="min-w-125px">Two-step</th>
-												<th class="min-w-125px">Joined Date</th>
+												<th class="min-w-125px">Full Name</th>
+												<th class="min-w-125px">Student No.</th>
+												<th class="min-w-125px">Email</th>
 												<th class="text-end min-w-100px">Actions</th>
 											</tr>
 											<!--end::Table row-->
@@ -624,109 +625,15 @@ if(!isset($_SESSION['email'])){
 													</div>
 												</td>
 												<!--end::Checkbox-->
-												<!--begin::User=-->
-												<td class="d-flex align-items-center">
-													<!--begin:: Avatar -->
-													<div class="symbol symbol-circle symbol-50px overflow-hidden me-3">
-														<a href="../../demo6/dist/apps/user-management/users/view.html">
-															<div class="symbol-label">
-																<img src="../assets/media/avatars/300-6.jpg" alt="Emma Smith" class="w-100" />
-															</div>
-														</a>
-													</div>
-													<!--end::Avatar-->
-													<!--begin::User details-->
-													<div class="d-flex flex-column">
-														<a href="../../demo6/dist/apps/user-management/users/view.html" class="text-gray-800 text-hover-primary mb-1">Emma Smith</a>
-														<span>smith@kpmg.com</span>
-													</div>
-													<!--begin::User details-->
-												</td>
-												<!--end::User=-->
-												<!--begin::Role=-->
-												<td>Administrator</td>
-												<!--end::Role=-->
-												<!--begin::Last login=-->
-												<td>
-													<div class="badge badge-light fw-bolder">Yesterday</div>
-												</td>
-												<!--end::Last login=-->
-												<!--begin::Two step=-->
-												<td></td>
-												<!--end::Two step=-->
-												<!--begin::Joined-->
-												<td>19 Aug 2022, 9:23 pm</td>
-												<!--begin::Joined-->
-												<!--begin::Action=-->
-												<td class="text-end">
-													<a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
-													<!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
-													<span class="svg-icon svg-icon-5 m-0">
-														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-															<path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor" />
-														</svg>
-													</span>
-													<!--end::Svg Icon--></a>
-													<!--begin::Menu-->
-													<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
-														<!--begin::Menu item-->
-														<div class="menu-item px-3">
-															<a href="../../demo6/dist/apps/user-management/users/view.html" class="menu-link px-3">Edit</a>
-														</div>
-														<!--end::Menu item-->
-														<!--begin::Menu item-->
-														<div class="menu-item px-3">
-															<a href="#" class="menu-link px-3" data-kt-users-table-filter="delete_row">Delete</a>
-														</div>
-														<!--end::Menu item-->
-													</div>
-													<!--end::Menu-->
-												</td>
-												<!--end::Action=-->
-											</tr>
-											<!--end::Table row-->
-											<!--begin::Table row-->
-											<tr>
-												<!--begin::Checkbox-->
-												<td>
-													<div class="form-check form-check-sm form-check-custom form-check-solid">
-														<input class="form-check-input" type="checkbox" value="1" />
-													</div>
-												</td>
-												<!--end::Checkbox-->
-												<!--begin::User=-->
-												<td class="d-flex align-items-center">
-													<!--begin:: Avatar -->
-													<div class="symbol symbol-circle symbol-50px overflow-hidden me-3">
-														<a href="../../demo6/dist/apps/user-management/users/view.html">
-															<div class="symbol-label fs-3 bg-light-danger text-danger">M</div>
-														</a>
-													</div>
-													<!--end::Avatar-->
-													<!--begin::User details-->
-													<div class="d-flex flex-column">
-														<a href="../../demo6/dist/apps/user-management/users/view.html" class="text-gray-800 text-hover-primary mb-1">Melody Macy</a>
-														<span>melody@altbox.com</span>
-													</div>
-													<!--begin::User details-->
-												</td>
-												<!--end::User=-->
-												<!--begin::Role=-->
-												<td>Analyst</td>
-												<!--end::Role=-->
-												<!--begin::Last login=-->
-												<td>
-													<div class="badge badge-light fw-bolder">20 mins ago</div>
-												</td>
-												<!--end::Last login=-->
-												<!--begin::Two step=-->
-												<td>
-													<div class="badge badge-light-success fw-bolder">Enabled</div>
-												</td>
-												<!--end::Two step=-->
-												<!--begin::Joined-->
-												<td>25 Jul 2022, 2:40 pm</td>
-												<!--begin::Joined-->
+												<!--begin::Name=-->
+												<td>Emma Smith</td>
+												<!--end::Name=-->
+												<!--begin::Student Number=-->
+												<td>ID-1234567</td>
+												<!--end::Student Number=-->
+												<!--begin::Email=-->
+												<td>es@gmail.com</td>
+												<!--end::Email=-->
 												<!--begin::Action=-->
 												<td class="text-end">
 													<a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
@@ -794,7 +701,11 @@ if(!isset($_SESSION['email'])){
 		<script src="../assets/plugins/global/plugins.bundle.js"></script>
 		<script src="../assets/js/scripts.bundle.js"></script>
 		<!--end::Global Javascript Bundle-->
+		<!--begin::Page Vendors Javascript(used by this page)-->
+		<script src="../assets/plugins/custom/datatables/datatables.bundle.js"></script>
+		<!--end::Page Vendors Javascript-->
 		<!--begin::Page Custom Javascript(used by this page)-->
+		<script src="../assets/js/custom/apps/user-management/users/list/table.js"></script>
 		<script src="../assets/js/custom/apps/user-management/users/list/add.js"></script>
 		<!--end::Page Custom Javascript-->
 		<!--end::Javascript-->
